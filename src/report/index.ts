14c7371bc61cd3377a22ed30279a5250f3f07471
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-08-28 19:48:18 +0900

    P10: export final report panel

--- src/report/index.ts
+++ src/report/index.ts
@@ -1,3 +1,4 @@
+export { FinalReportPanel } from './FinalReportPanel';
 export { loadVerifiedExperimentExport, parseFrozenRegistration, type VerifiedExperimentExport } from './export';
 export { finalExperimentDate, projectFinalLayerC, projectReportSessions, type FinalLayerCProjection, type ReportSession } from './ledgerProjection';
 export { FINAL_REPORT_LIMITATIONS, buildFinalReportModel } from './model';
