7d9509c82ac90b000e40cec0cd2faddcb75986d5
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-08-28 11:40:55 +0900

    P6: export dashboard module

---
+++ src/dashboard/index.ts
@@ -0,0 +1,9 @@
+export { ExperimentDashboard } from './ExperimentDashboard';
+export {
+  CONDITION_LABELS,
+  buildLayerADashboardModel,
+  type CalendarDay,
+  type DashboardConditionCard,
+  type LayerADashboardModel,
+  type MiracleLogItem,
+} from './model';
