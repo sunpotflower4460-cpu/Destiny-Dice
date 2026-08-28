ead5b3f20349e8ea0149f535398fd5f15ef974d5
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-08-28 19:43:29 +0900

    P10: export final report API

---
+++ src/report/index.ts
@@ -0,0 +1,5 @@
+export { loadVerifiedExperimentExport, parseFrozenRegistration, type VerifiedExperimentExport } from './export';
+export { finalExperimentDate, projectFinalLayerC, projectReportSessions, type FinalLayerCProjection, type ReportSession } from './ledgerProjection';
+export { FINAL_REPORT_LIMITATIONS, buildFinalReportModel } from './model';
+export { renderFinalReportMarkdown } from './markdown';
+export type { EvidenceGrade, FinalLayerCReport, FinalReportModel, PredictionAnswerCheck, WishTextFeatureSummary } from './types';
